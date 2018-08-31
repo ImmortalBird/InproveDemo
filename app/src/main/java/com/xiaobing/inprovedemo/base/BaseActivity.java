dc4d49458ec48d80eedaa3a78ba193b00ae400e1
ImmortalBird <471342365@qq.com>
2018-08-31 11:33:37 +0800

    debug 1.0

---
+++ app/src/main/java/com/xiaobing/inprovedemo/base/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.xiaobing.inprovedemo.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2018/8/29 0029.
+ */
+
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/xiaobing/inprovedemo/notification/NotificationActivity.java
@@ -0,0 +1,16 @@
+package com.xiaobing.inprovedemo.notification;
+
+import android.os.Bundle;
+
+import com.xiaobing.inprovedemo.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/8/29 0029.
+ */
+
+public class NotificationActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
