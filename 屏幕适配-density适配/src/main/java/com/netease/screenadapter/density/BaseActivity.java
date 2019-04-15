9eddf3c077c04f92a94b7c1a8a3f27760edbdf27
常晓冰 <471342365@qq.com>
2019-04-15 18:08:28 +0800

    增加几个示例项目

---
+++ 屏幕适配-density适配/src/main/java/com/netease/screenadapter/density/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.netease.screenadapter.density;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        Density.setDensity(getApplication(),this);
+    }
+}
---
+++ 屏幕适配-density适配/src/main/java/com/netease/screenadapter/density/my/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.netease.screenadapter.density.my;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author 常晓冰
+ * @E-mail 471342365@qq.com
+ * @date Created on 2019/4/15
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
