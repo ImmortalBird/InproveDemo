090a7e591fc86f9ea8b6942c352e9dfeb5f7cc80
Joker <changxiaobing@coojr.com>
2020-08-04 11:07:39 +0800

    各种工具类，未完成

--- app/src/main/java/com/xiaobing/improvedemo/util/ClipboardUtil.java
+++ app/src/main/java/com/xiaobing/improvedemo/util/ClipboardUtil.java
@@ -7,36 +7,59 @@ import android.content.Context;
 import android.util.Log;
 
 /**
-* Created by Joker. 2020/7/29 11:37
-*
-* @E-mail: xiaobing.joker@qq.com
-* @Description:
-*/
+ * Created by Joker. 2020/7/29 11:37
+ *
+ * @E-mail: xiaobing.joker@qq.com
+ * 操作剪贴板的工具类
+ */
 class ClipboardUtil {
-   private static final String TAG = "ClipboardUtil";
+    private static final String TAG = "ClipboardUtil";
 
-   /**
-    * 获取剪贴板中第一条内容
-    * @param activity 上下文对象
-    * @return 剪贴板第一条的内容，如果剪贴板为空，返回 "-1"
-    */
-   public static String getPasteString(final Activity activity) {
-       // 获取剪贴板内容
+    /**
+     * 获取剪贴板中第一条内容
+     *
+     * @param activity 上下文对象
+     * @return 剪贴板第一条的内容，如果剪贴板为空，返回 "-1"
+     */
+    public static String getPasteString(final Activity activity) {
+        // 获取剪贴板内容
 
-       Log.d(TAG, "Thread.currentThread().getId()" + Thread.currentThread().getId());
+        Log.d(TAG, "Thread.currentThread().getId()" + Thread.currentThread().getId());
 
-       ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
-       ClipData clipData = null;
-       if (clipboard != null) {
-           clipData = clipboard.getPrimaryClip();
-       }
-       if (clipData != null && clipData.getItemCount() > 0) {
-           CharSequence text = clipData.getItemAt(0).getText();
-           Log.d(TAG, "getFromClipboard text=" + text.toString());
-           return text.toString();
-       }
-       return "-1";
+        ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clipData = null;
+        if (clipboard != null) {
+            clipData = clipboard.getPrimaryClip();
+        }
+        if (clipData != null && clipData.getItemCount() > 0) {
+            CharSequence text = clipData.getItemAt(0).getText();
+            Log.d(TAG, "getFromClipboard text=" + text.toString());
+            return text.toString();
+        }
+        return "-1";
+    }
 
-   }
+    /**
+     * 向剪贴板设置数据
+     *
+     * @param context   content
+     * @param data      要设置到剪贴板的内容
+     */
+    public static void setClipData(final Context context, String data) {
+        setClipData(context, data, "");
+    }
 
+
+    /**
+     * 向剪贴板设置数据
+     *
+     * @param context   content
+     * @param data      要设置到剪贴板的内容
+     * @param label     描述
+     */
+    public static void setClipData(final Context context, String data, String label) {
+        ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        if (clipboardManager != null)
+            clipboardManager.setPrimaryClip(ClipData.newPlainText(label, data));
+    }
 }
