877a6b5db92f2bb8c22d553f610078aed0ebf078
Joker <471342365@qq.com>
2022-05-07 21:59:23 +0800

    .

--- test2/src/main/java/com/example/test2/MyViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.test2
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class MyViewModel : ViewModel() {
-
-    val name = MutableLiveData<String>("")
-}
